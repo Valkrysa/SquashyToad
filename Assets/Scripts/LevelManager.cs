1477ae411ef8fb38233d70487f6b81cc0394055e
Alice Robinson <valkrysa@gmail.com>
2017-09-04 19:18:42 -0700

    Interacting with Buttons in VR

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void LoadNextLevel () {
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
+	}
+
+	public void QuitGame () {
+		Debug.Log ("Game will quit.");
+		Application.Quit ();
+	}
+}
